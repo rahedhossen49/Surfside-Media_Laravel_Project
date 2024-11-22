872ac34a61a15a12c2312c15899fd6622afdb1d5
rahedhossen49 <rahedhossen49@gmail.com>
2024-11-22 22:56:23 +0600

    first commit

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -75,15 +75,27 @@ class ShopController extends Controller
     }
 
 
-
-
-
     public function product_details($product_slug)
     {
         $product = Product::where('slug', $product_slug)->first();
-        $rproducts = Product::where('slug', '<>', $product_slug)->get()->take(8);
+
+        $rproducts = Product::where('category_id', $product->category_id)
+            ->where('slug', '<>', $product_slug)
+            ->take(8)
+            ->get();
+
         return view('details', compact('product', 'rproducts'));
     }
+
+
+
+
+    // public function product_details($product_slug)
+    // {
+    //     $product = Product::where('slug', $product_slug)->first();
+    //     $rproducts = Product::where('slug', '<>', $product_slug)->get()->take(8);
+    //     return view('details', compact('product', 'rproducts'));
+    // }
 
     public function reviewStore(Request $request)
     {
--- resources/views/details.blade.php
+++ resources/views/details.blade.php
@@ -2,7 +2,9 @@
 
 
 @section('content')
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css"
+        integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <style>
         .filled-heart {
             color: orange;
@@ -498,6 +500,9 @@
                         @endforeach
 
                     </div><!-- /.swiper-wrapper -->
+
+
+
                 </div><!-- /.swiper-container js-swiper-slider -->
 
                 <div
